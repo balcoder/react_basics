2e218d7c2fe32caf04612c1094916c62aa411c6b
Des Barrett <des.barrett@gmail.com>
2018-01-08 11:46:38 +0000

    fixed typo in Home.js

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -30,14 +30,14 @@ export class Home extends React.Component {
     console.log(this.props);
     return(
         <div>
-          <p>I'm a new component</p>
+          <p>A new component</p>
           <p>Your name is {this.props.name} , your age is {this.state.age}</p>
           <hr/>
           <button onClick={this.onMakeOlder.bind(this)} className="btn btn-primary">Make me older!</button>
           <hr/>
           <button onClick={this.props.greet} className="btn btn-primary">Greet</button>
           <hr/>
-          <input type="test" value={this.state.homeLink}
+          <input type="text" value={this.state.homeLink}
             // use es6 syntax to avoid writing bind this
             onChange={(event) => this.onHandleChange(event)}/>
           <button onClick={this.onChangeLink.bind(this)}className="btn btn-primary">Change the Home Link</button>
--- src/app/index.js
+++ src/app/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 
 
+
 import { Header } from "./components/Header";
 import { Home } from "./components/Home";
 
